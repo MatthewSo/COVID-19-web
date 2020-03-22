7834f19e415289a1fede83462b053f24c2cf4856
MatthewSo <MatthewSo>
2020-03-22 16:13:34 -0400

    Created BlogLib

---
+++ blogLib.py
@@ -0,0 +1,27 @@
+import pickle
+
+blog_file = "blog.pkl"
+
+blog_user="miso"
+blog_pass='-905124903459320104'
+
+
+def save_blog(posts):
+    with open(blog_file, 'wb') as f:
+        pickle.dump(posts, f, pickle.HIGHEST_PROTOCOL)
+
+def load_blog():
+    with open(blog_file, 'rb') as f:
+        return pickle.load(f)
+
+def add_blog_post(author, title, content, date):
+    posts_temp = load_blog()
+    posts_temp.insert(0,{'author':author, 'title':title,'content':content,'date':date})
+    save_blog(posts_temp)
+
+def delete_blog_post(title):
+    posts_temp = load_blog()
+    ret = [i for i in posts_temp if not (i['title'] == title)] 
+    save_blog(ret)
+
+def load_posts():
